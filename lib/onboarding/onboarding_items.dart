55660ad66f23f1b3296d66f27da25e2414c478fd
Ravshanbek Odilboyev <ravshaninfo@gmail.com>
2024-06-02 00:00:04 +0500

    first commit

---
+++ lib/onboarding/onboarding_items.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'onboarding_info.dart';
+
+class OnboardingItems {
+  List<OnboardingInfo> items = [
+    OnboardingInfo(
+        title: "About Me",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+        image: "assets/Object.png",
+    ),
+    OnboardingInfo(
+      title: "title",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+      image: "assets/Object2.png",
+    ),
+    OnboardingInfo(
+      title: "title",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+      image: "assets/Object3.png",
+    ),
+    OnboardingInfo(
+      title: "title",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+      image: "assets/Object2.png",
+    ),
+  ];
+}
---
+++ lib/onboarding/onboarding_view.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:onboarding_screen/home.dart';
+import 'package:onboarding_screen/onboarding/onboarding_items.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class OnboardingView extends StatefulWidget {
+  const OnboardingView({super.key});
+
+  @override
+  State<OnboardingView> createState() => _OnboardingViewState();
+}
+
+class _OnboardingViewState extends State<OnboardingView> {
+  final controller = OnboardingItems();
+  final pageController = PageController();
+
+  bool isLastPage = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomSheet: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: isLastPage ? getStarted() : Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextButton(
+                onPressed: () => pageController.jumpToPage(controller.items.length-1),
+                child: const Text("Skip"),
+            ),
+
+            SmoothPageIndicator(
+                controller: pageController,
+                count: controller.items.length,
+                onDotClicked: (index) => pageController.animateToPage(index,
+                    duration: const Duration(milliseconds: 600), curve: Curves.easeIn),
+                effect: const WormEffect(
+                  dotColor: Colors.grey,
+                  dotHeight: 12,
+                  dotWidth: 12,
+                  activeDotColor: Colors.blue,
+                ),
+            ),
+
+            TextButton(
+              onPressed: () => pageController.nextPage(
+                  duration: const Duration(milliseconds: 600),
+                  curve: Curves.easeIn),
+              child: const Text("Next"),
+            )
+          ],
+        ),
+      ),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 15),
+        child: PageView.builder(
+            onPageChanged: (index) => setState(() => isLastPage = controller.items.length-1 == index),
+            itemCount: controller.items.length,
+            controller: pageController,
+            itemBuilder: (context, index){
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(controller.items[index].image),
+                  const SizedBox(height: 15,),
+                  Text(controller.items[index].title, style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+                  const SizedBox(height: 15,),
+                  Text(controller.items[index].description, style: const TextStyle(color: Colors.grey, fontSize: 17), textAlign: TextAlign.center,),
+                ],
+              );
+            }
+        ),
+      ),
+    );
+  }
+
+  Widget getStarted(){
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: Colors.blue,
+      ),
+      width: MediaQuery.of(context).size.width * .9,
+      height: 55,
+      child: TextButton(
+        onPressed: ()async{
+          final press = await SharedPreferences.getInstance();
+          press.setBool("onboarding", true);
+
+
+          if(!mounted)return;
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Home()));
+        },
+        child: const Text("Get Started", style: TextStyle(color: Colors.white),),
+      ),
+    );
+  }
+
+}
